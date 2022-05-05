6a8b8da19b6a31327b3cf36669ac64c528fa81eb
Toph Bantoto <lcbantoto@gmail.com>
2022-05-05 15:03:19 -0400

    working deploy

--- venv/app.py
+++ venv/app.py
@@ -175,6 +175,6 @@ def dailyGuess():
     return resp
 
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=80)
-    # app.run(debug=True, use_reloader=True)
+    # from waitress import serve
+    # serve(app, host="0.0.0.0", port=80)
+    app.run(debug=True, use_reloader=True)
